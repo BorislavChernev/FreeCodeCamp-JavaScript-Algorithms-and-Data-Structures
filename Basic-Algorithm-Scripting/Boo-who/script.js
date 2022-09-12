7aeaec3e595cce6b573fc348f6cfcc3b3f22ef73
BorislavChernev <borocool359@gmail.com>
2022-09-12 16:31:53 +0300

    Completed Task

---
+++ Basic-Algorithm-Scripting/Boo-who/script.js
@@ -0,0 +1,5 @@
+function booWho(bool) {
+    return bool === true || bool === false ? true : false;
+  }
+  
+  booWho(null);
---
+++ Basic-Algorithm-Scripting/Confirm-the-Ending/script.js
@@ -0,0 +1,7 @@
+function confirmEnding(str, target) {
+
+    return str.slice(str.length - target.length) === target;
+    
+  }
+  
+  confirmEnding("Bastian", "n");
