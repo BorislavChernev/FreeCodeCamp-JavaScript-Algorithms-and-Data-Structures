296e36d6b467aabcbdb6a472976feec30d55d228
BorislavChernev <borocool359@gmail.com>
2022-09-24 23:33:57 +0300

    Completed Task

---
+++ Debugging/Use-the-JavaScript-Console-to-Check-the-Value-of-a-Variable/script.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 1;
+a++;
+
+
+let sumAB = a + b;
+console.log(a);
---
+++ Debugging/Use-typeof-to-Check-the-Type-of-a-Variable/script.js
@@ -0,0 +1,5 @@
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven);
+console.log(typeof three);
