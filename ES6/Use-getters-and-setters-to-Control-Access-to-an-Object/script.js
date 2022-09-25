5698e8049fdc717b5bedd4154a139501dbb06c46
BorislavChernev <borocool359@gmail.com>
2022-09-25 13:19:13 +0300

    Completed Task

---
+++ ES6/Use-getters-and-setters-to-Control-Access-to-an-Object/script.js
@@ -0,0 +1,13 @@
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
