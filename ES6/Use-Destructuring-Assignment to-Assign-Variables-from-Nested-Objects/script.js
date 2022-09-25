c3cc5bf8bf33a2c9d22e5972257769385c150a6f
BorislavChernev <borocool359@gmail.com>
2022-09-25 13:18:56 +0300

    Completed Task

---
+++ ES6/Use-Destructuring-Assignment to-Assign-Variables-from-Nested-Objects/script.js
@@ -0,0 +1,8 @@
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  const { today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
+  
