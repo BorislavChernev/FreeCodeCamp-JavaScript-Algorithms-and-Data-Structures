fb409fc839d8d48898037f8a7d997b12f8d4075d
BorislavChernev <borocool359@gmail.com>
2022-09-28 18:11:46 +0300

    Completed Task

---
+++ Intermediate-Algorithm-Scripting/Search-and-Replace.js
@@ -0,0 +1,9 @@
+function myReplace(str, before, after) {
+	if (/^[A-Z]/.test(before)) {
+		after = after[0].toUpperCase() + after.substring(1);
+	} else {
+		after = after[0].toLowerCase() + after.substring(1);
+	}
+
+	return str.replace(before, after);
+}
