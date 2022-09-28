be058ba5a9d4dac5b12db4e3c3c85deefd0c4639
BorislavChernev <borocool359@gmail.com>
2022-09-28 18:11:02 +0300

    Completed Task

---
+++ Intermediate-Algorithm-Scripting/Make-a-Person.js
@@ -0,0 +1,30 @@
+const Person = function (firstAndLast) {
+	let fullName = firstAndLast;
+
+	this.getFirstName = function () {
+		return fullName.split(' ')[0];
+	};
+
+	this.getLastName = function () {
+		return fullName.split(' ')[1];
+	};
+
+	this.getFullName = function () {
+		return fullName;
+	};
+
+	this.setFirstName = function (name) {
+		fullName = name + ' ' + fullName.split(' ')[1];
+	};
+
+	this.setLastName = function (name) {
+		fullName = fullName.split(' ')[0] + ' ' + name;
+	};
+
+	this.setFullName = function (name) {
+		fullName = name;
+	};
+};
+
+const bob = new Person('Bob Ross');
+console.log(bob.getFullName());
