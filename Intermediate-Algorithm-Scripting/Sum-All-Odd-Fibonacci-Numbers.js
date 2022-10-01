bf4c8d5e4faa85921904b615449f4660ce688c9f
BorislavChernev <borocool359@gmail.com>
2022-10-01 17:28:48 +0300

    Completed Task

---
+++ Intermediate-Algorithm-Scripting/Sum-All-Odd-Fibonacci-Numbers.js
@@ -0,0 +1,12 @@
+function sumFibs(num) {
+	if (num <= 0) return 0;
+
+	const arrFib = [1, 1];
+	let nextFib = 0;
+
+	while ((nextFib = arrFib[0] + arrFib[1]) <= num) {
+		arrFib.unshift(nextFib);
+	}
+
+	return arrFib.filter((x) => x % 2 != 0).reduce((a, b) => a + b);
+}
